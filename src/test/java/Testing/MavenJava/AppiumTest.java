549c2e28b5e847c975be5118f7edc87007f399a6
Pavithra <pavithrashankar4698@gmail.com>
2022-02-09 13:59:49 +0530

    American commit development

--- src/test/java/Testing/MavenJava/AppiumTest.java
+++ src/test/java/Testing/MavenJava/AppiumTest.java
@@ -7,12 +7,37 @@ public class AppiumTest {
 	@Test
 	public void NativeApp() {
 		System.out.println("qqqq");
+		System.out.println("aaaa");
+		System.out.println("aaaa");
+		System.out.println("aaaa");
+		System.out.println("aaaa");
 
 		
 		
 	}@Test
 	public void IOSApp() {
 		System.out.println("rrrrr");
+		System.out.println("aaaa");
+		System.out.println("aaaa");
+		System.out.println("aaaa");
+
+		
+	}
+	@Test
+	public void IOSApp1() {
+		System.out.println("rrrrr");
+		System.out.println("aaaa");
+		System.out.println("aaaa");
+		System.out.println("aaaa");
+
+		
+	}
+	@Test
+	public void IOSApp2() {
+		System.out.println("rrrrr");
+		System.out.println("aaaa");
+		System.out.println("aaaa");
+		System.out.println("aaaa");
 
 		
 	}
